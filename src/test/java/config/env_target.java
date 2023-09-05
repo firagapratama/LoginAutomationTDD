a4f9f6057c0895b0ad10b65bfd2048f368a42343
Firaga Pratama <firagap@gmail.com>
2023-09-05 10:05:31 +0700

    Update env_target.java

--- src/test/java/config/env_target.java
+++ src/test/java/config/env_target.java
@@ -4,5 +4,5 @@ import org.openqa.selenium.WebDriver;
 
 public class env_target {
     protected WebDriver driver;
-    protected String baseUrl = "https://sidomuncul.arkamaya.net/";
+    protected String baseUrl = "{your_url}";
 }
